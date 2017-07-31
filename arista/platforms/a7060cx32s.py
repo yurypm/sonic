155861e92766d5a177ae9863bd708585d3ce4594
Samuel Angebault <staphylo@arista.com>
2017-07-31 16:42:32 -0700

    Add support for DCS-7060CX-32S-ES
    Change-Id: Ibcd0dd0368c643fbbf269979e2445a336d55d17f

--- arista/platforms/a7060cx32s.py
+++ arista/platforms/a7060cx32s.py
@@ -7,7 +7,7 @@ from ..components.common import I2cKernelComponent
 from ..components.scd import Scd
 
 
-@registerPlatform('DCS-7060CX-32S')
+@registerPlatform(['DCS-7060CX-32S', 'DCS-7060CX-32S-ES'])
 class Upperlake(Platform):
    def __init__(self):
       super(Upperlake, self).__init__()
